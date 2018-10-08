c83126a7191b190b64dbdcdfbbcad56c62874ac6
Tim Miller <timothy.miller@childrens.harvard.edu>
2018-10-08 09:52:07 -0400

    Simplify run script with indices. Remove some crlfs.

--- AE-SCL/run.py
+++ AE-SCL/run.py
@@ -3,9 +3,12 @@ import sentiment
 if __name__ == '__main__':
     domain = []
     domain.append("books")
-    domain.append("kitchen")
     domain.append("dvd")
     domain.append("electronics")
+    domain.append("kitchen")
+
+    source_ind = 2
+    target_ind = 3
 
     # making a shared representation for both source domain and target domain
     # first param: the source domain
@@ -14,7 +17,7 @@ if __name__ == '__main__':
     # fourth param: appearance threshold for pivots in source and target domain
     # fifth parameter: the embedding dimension, identical to the hidden layer dimension
 
-    tr.train(domain[0], domain[1], 100, 10, 500)
+    tr.train(domain[source_ind], domain[target_ind], 100, 10, 500)
 
     # learning the classifier in the source domain and testing in the target domain
     # the results, weights and all the meta-data will appear in source-target directory
@@ -25,5 +28,6 @@ if __name__ == '__main__':
     # fifth param: the embedding dimension identical to the hidden layer dimension
     # sixth param: we use logistic regression as our classifier, it takes the const C for its learning
 
-    sentiment.sent(domain[0], domain[1], 100, 10, 500, 0.1)
+    sentiment.sent(domain[source_ind], domain[target_ind], 100, 10, 500, 0.1)
+    print("Running AE-SCL with source domain %s and target domain %s" % (domain[source_ind], domain[target_ind]))
 
