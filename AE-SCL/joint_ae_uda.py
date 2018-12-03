b108e4f1a902230f147b0a6eb8c3782b414ca9e3
Tim Miller <tim.miller@gmail.com>
2018-12-03 07:32:51 -0500

    Added domain and task outputs to AE side.

--- AE-SCL/joint_ae_uda.py
+++ AE-SCL/joint_ae_uda.py
@@ -13,11 +13,25 @@ import torch.optim as optim
 import torch
 from torch import sigmoid
 from torch.nn.functional import relu
+from torch.autograd import Function
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 
 from pre import XML2arrayRAW, GetTopNMI
+
+class ReverseLayerF(Function):
+    @staticmethod
+    def forward(ctx, x, alpha):
+        ctx.alpha = alpha
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # reversed (default)
+        output = grad_output.neg() * ctx.alpha
+        # print("Input grad is %s, output grad is %s" % (grad_output.data.cpu().numpy()[:10], output.data.cpu().numpy()[:10]))
+        return output, None
 
 class JointLearnerModel(nn.Module):
 
@@ -35,8 +49,11 @@ class JointLearnerModel(nn.Module):
         #self.pivot_ae = nn.Sequential()
         self.rep_projector = nn.Linear(non_pivot_candidates, pivot_hidden_nodes)
         self.rep_predictor = nn.Linear(pivot_hidden_nodes, num_pivot_candidates)
+
+        self.task2_classifier = nn.Linear(num_pivot_candidates,1)
+        self.dom_classifier = nn.Linear(num_pivot_candidates,1)
         
-    def forward(self, full_input, pivot_input):
+    def forward(self, full_input, pivot_input, alpha=1.0):
 
         # Get predictions for all pivot candidates:
         pivot_rep = sigmoid(self.rep_projector(pivot_input))
@@ -46,9 +63,15 @@ class JointLearnerModel(nn.Module):
         
         # Get task prediction
         task_prediction = self.task_classifier(task_input)
+
+        task_prediction2 = self.task2_classifier(pivot_pred)
+ 
+        # Pass through gradient reversal layer and make domain prediction
+        dom_prediction = self.dom_classifier( ReverseLayerF.apply(pivot_pred, alpha) )
+
         # oracle_prediction = self.oracle_classifier(task_input)
 
-        return task_prediction, pivot_pred, pivot_rep
+        return task_prediction, pivot_pred, pivot_rep, task_prediction2, dom_prediction
 
 def get_shuffled(X_feats, X_ae=None, y=None):
     inds = np.arange(X_feats.shape[0])
@@ -64,21 +87,23 @@ def get_shuffled(X_feats, X_ae=None, y=None):
         shuffled_y = None
     else:
         shuffled_y = y[inds]
-    return shuffled_X_feats, shuffled_X_ae, shuffled_y
+    return shuffled_X_feats, shuffled_X_ae, shuffled_y, inds
 
 def log(msg):
     sys.stdout.write('%s\n' % msg)
     sys.stdout.flush()
 
-def train_model(X_source_feats, X_source_ae, y_source, X_target_feats, X_target_ae, ae_input_inds, ae_output_inds, y_target=None, X_unlabeled_feats=None, X_unlabeled_ae=None,  X_source_valid_feats=None, X_source_valid_ae=None, y_source_valid=None):
+def train_model(X_source_feats, X_source_ae, y_source, X_target_feats, X_target_ae, ae_input_inds, ae_output_inds, y_target=None, X_unlabeled_feats=None, X_unlabeled_ae=None,  y_unlabeled_dom=None, X_source_valid_feats=None, X_source_valid_ae=None, y_source_valid=None):
     assert X_source_feats.shape[1] == X_target_feats.shape[1], "Source and target training data do not have the same number of features!"
     assert X_source_feats.shape[1] == X_unlabeled_feats.shape[1], "Source and unlabeled training data do not have the same number of features!"
 
     device='cuda' if torch.cuda.is_available() else 'cpu'
  
-    epochs = 25
+    epochs = 30
     recon_weight = 100.0
-    l2_weight = 0.0 #1
+    l2_weight = 0.1 #1
+    t2_weight = 0.100
+    dom_weight = 0.10000
     # oracle_weight = 1.0
     max_batch_size = 50
     pivot_hidden_nodes = 500
@@ -111,10 +136,13 @@ def train_model(X_source_feats, X_source_ae, y_source, X_target_feats, X_target_
 
     model = JointLearnerModel(num_features, len(ae_input_inds), len(ae_output_inds), pivot_hidden_nodes=pivot_hidden_nodes).to(device)
     task_lossfn = nn.BCEWithLogitsLoss().to(device)
+    task2_lossfn = nn.BCEWithLogitsLoss().to(device)
+    dom_lossfn = nn.BCEWithLogitsLoss().to(device)
     recon_lossfn = nn.BCEWithLogitsLoss().to(device)
     l2_lossfn = nn.MSELoss(reduction='sum').to(device)
 
-    opt = optim.Adam(model.parameters(), weight_decay=weight_decay, lr=lr) 
+    opt = optim.Adam(model.parameters(), weight_decay=weight_decay, lr=lr)
+    sched = optim.lr_scheduler.ReduceLROnPlateau(opt, 'max', patience=5, factor=0.33)
     best_valid_acc = 0
 
     for epoch in range(epochs):
@@ -127,23 +155,34 @@ def train_model(X_source_feats, X_source_ae, y_source, X_target_feats, X_target_
         true_preds = 0      # denom for prec
         correct_preds = 0   # For accuracy
 
-        source_X, source_X_ae, source_y = get_shuffled(X_source_feats, X_source_ae, y_source)
-        target_X, target_X_ae, target_y = get_shuffled(X_target_feats, X_target_ae, y_target)
-        unlabeled_X, unlabeled_X_ae,_ = get_shuffled(X_unlabeled_feats, X_unlabeled_ae)
+        source_X, source_X_ae, source_y,_ = get_shuffled(X_source_feats, X_source_ae, y_source)
+        target_X, target_X_ae, target_y,_ = get_shuffled(X_target_feats, X_target_ae, y_target)
+        unlabeled_X, unlabeled_X_ae,_,unlabeled_inds = get_shuffled(X_unlabeled_feats, X_unlabeled_ae)
+        batch_unlabeled_dom_y = y_unlabeled_dom[unlabeled_inds]
 
         for batch in range(num_batches):
             model.zero_grad()
+
+            # ave_ind = source_batch_ind + source_batch_size // 2
+            # p = float(ave_ind + epoch * num_source_instances*2) / (epochs * num_source_instances*2)
+            # alpha = 2. / (1. + np.exp(-10 * p)) - 1
+            alpha = 1.0
 
             # Pass it source examples and compute task loss and pivot reconstruction loss:
             batch_source_X = torch.FloatTensor(source_X[source_batch_ind:source_batch_ind+source_batch_size, :].toarray()).to(device)
 
             ae_inputs = torch.FloatTensor(source_X_ae[source_batch_ind:source_batch_ind+source_batch_size,ae_input_inds].toarray()).to(device)
 
-            task_pred,pivot_pred,_ = model(batch_source_X, ae_inputs)
+            task_pred,pivot_pred,_,task2_pred,dom_pred = model(batch_source_X, ae_inputs, alpha=alpha)
 
             # Get task loss:
             batch_source_y = torch.FloatTensor(source_y[source_batch_ind:source_batch_ind+source_batch_size]).to(device).unsqueeze(1)
             task_loss = task_lossfn(task_pred, batch_source_y)
+            task2_loss = task2_lossfn(task2_pred, batch_source_y)
+
+            # Get domain loss:
+            batch_source_dom_y = torch.zeros_like(batch_source_y) + 1
+            dom_loss = dom_lossfn(dom_pred, batch_source_dom_y)
 
             # Get reconstruction loss:
             ae_outputs = torch.FloatTensor(source_X_ae[source_batch_ind:source_batch_ind+source_batch_size,ae_output_inds].toarray()).to(device)
@@ -161,24 +200,26 @@ def train_model(X_source_feats, X_source_ae, y_source, X_target_feats, X_target_
             batch_target_X = torch.FloatTensor(target_X[target_batch_ind:target_batch_ind+target_batch_size, :].toarray()).to(device)
             ae_inputs = torch.FloatTensor(target_X_ae[target_batch_ind:target_batch_ind+target_batch_size,ae_input_inds].toarray()).to(device)
 
-            target_task_pred,pivot_pred,_ = model(batch_target_X, ae_inputs)
+            target_task_pred,pivot_pred,_,_,dom_pred = model(batch_target_X, ae_inputs, alpha=alpha)
 
             # No task loss because we don't have target labels
+
+            # domain loss:
+            batch_target_dom_y = torch.zeros_like(dom_pred)
+            dom_loss += dom_lossfn(dom_pred, batch_target_dom_y)
 
             # Reconstruction loss:
             ae_outputs = torch.FloatTensor(target_X_ae[target_batch_ind:target_batch_ind+target_batch_size,ae_output_inds].toarray()).to(device)
             target_recon_loss = recon_lossfn(pivot_pred, ae_outputs)
 
             # do representation learning on the unlabeled instances
-            # batch_unlabeled_X = unlabeled_X[un_batch_ind:un_batch_ind+un_batch_size, :]
-            # if un_batch_size > max_batch_size:
             num_sub_batches = 1 + (un_batch_size // max_batch_size)
             sub_batch_start_ind = 0
             unlabeled_recon_loss = 0.0
             for sub_batch in range(num_sub_batches):
                 sub_batch_size = min(max_batch_size, un_batch_size - sub_batch*max_batch_size )
                 if sub_batch_size <= 0:
-                    print('Found an edge case where sub_batch_size<=0 with un_batch_size=%d' % (un_batch_size))
+                    log('Found an edge case where sub_batch_size<=0 with un_batch_size=%d' % (un_batch_size))
                     break
                 #if sub_batch_size < max_batch_size:
                     #print('Batch %d has size %d' % (sub_batch, sub_batch_size))
@@ -186,16 +227,22 @@ def train_model(X_source_feats, X_source_ae, y_source, X_target_feats, X_target_
                 sub_batch_unlabeled_X = torch.FloatTensor(unlabeled_X[un_batch_ind+sub_batch_start_ind:un_batch_ind+sub_batch_start_ind+sub_batch_size, :].toarray()).to(device)
                 ae_inputs = torch.FloatTensor(unlabeled_X_ae[un_batch_ind+sub_batch_start_ind:un_batch_ind+sub_batch_start_ind+sub_batch_size,ae_input_inds].toarray()).to(device)
                 ae_outputs = torch.FloatTensor(unlabeled_X_ae[un_batch_ind+sub_batch_start_ind:un_batch_ind+sub_batch_start_ind+sub_batch_size, ae_output_inds].toarray()).to(device)
-                _, pivot_pred,_ = model(sub_batch_unlabeled_X, ae_inputs)
+                _, pivot_pred,_,_,dom_pred = model(sub_batch_unlabeled_X, ae_inputs, alpha=alpha)
+                sub_batch_dom_y = torch.FloatTensor( batch_unlabeled_dom_y[un_batch_ind+sub_batch_start_ind:un_batch_ind+sub_batch_start_ind+sub_batch_size]).to(device)
+                dom_loss += dom_lossfn(dom_pred, sub_batch_dom_y)
                 unlabeled_recon_loss += recon_lossfn(pivot_pred, ae_outputs)
                 sub_batch_start_ind += max_batch_size
 
-
-            # Compute the total loss and step the optimizer in the right direction:
             l2_loss = l2_lossfn( model.task_classifier.weight, torch.zeros_like(model.task_classifier.weight))
+            # Compute the total loss and step the optimizer in the right direction:
+            if batch == 0:
+                log('Epoch %d: task=%f l2=%f task2=%f dom=%f src_recon=%f, tgt_recon=%f, un_recon=%f' % 
+                    (epoch, task_loss, l2_loss, task2_loss, dom_loss, source_recon_loss, target_recon_loss, unlabeled_recon_loss))
             total_loss = (task_loss + 
                          l2_weight * l2_loss +
-                         recon_weight * (source_recon_loss + target_recon_loss + unlabeled_recon_loss))
+                         t2_weight * task2_loss +
+                         dom_weight * dom_loss +
+                         recon_weight * (target_recon_loss + unlabeled_recon_loss))
             epoch_loss += total_loss.item()
             total_loss.backward()
      
@@ -207,6 +254,7 @@ def train_model(X_source_feats, X_source_ae, y_source, X_target_feats, X_target_
 
         # Print out some useful info: losses, weights of pivot filter, accuracy
         acc = correct_preds / source_batch_ind
+
         log("Epoch %d finished: loss=%f" % (epoch, epoch_loss) )
         log("  Training accuracy=%f" % (acc))
 
@@ -219,6 +267,11 @@ def train_model(X_source_feats, X_source_ae, y_source, X_target_feats, X_target_
             acc = correct_preds / len(y_source_valid)
 
             log("  Validation accuracy=%f" % (acc))
+            # sched.step(acc)
+            new_lr = [ group['lr'] for group in opt.param_groups ][0]
+            if new_lr != lr:
+                log("Learning rate modified to %f" % (new_lr))
+                lr = new_lr
 
             if acc > best_valid_acc:
                 sys.stderr.write('Writing model at epoch %d\n' % (epoch))
@@ -226,7 +279,7 @@ def train_model(X_source_feats, X_source_ae, y_source, X_target_feats, X_target_
                 best_valid_acc = acc
                 torch.save(model, 'best_model.pt')
 
-        del unlabeled_X
+        del unlabeled_X, unlabeled_X_ae
 
 domains = ['books', 'dvd', 'electronics', 'kitchen']
 parser = argparse.ArgumentParser(description='PyTorch joint domain adaptation neural network trainer')
@@ -264,7 +317,7 @@ def main(args):
             test_labels = pickle.load(f)
 
     # gets all the train and test for pivot classification
-    unlabeled_only,source_only,target=XML2arrayRAW("data/"+src+"/"+src+"UN.txt","data/"+dest+"/"+dest+"UN.txt")
+    unlabeled_only,source_only,target_un=XML2arrayRAW("data/"+src+"/"+src+"UN.txt","data/"+dest+"/"+dest+"UN.txt")
     source_all=source_only+train
     un_count = 40
     num_pivots = 100   # Only used in configuration that uses MI to get pivots
@@ -274,18 +327,24 @@ def main(args):
     dest_test, _,_ = XML2arrayRAW("data/"+dest+"/negative.parsed","data/"+dest+"/positive.parsed")
     dest_test_labels= [0]*lbl_num+[1]*lbl_num
 
-    unlabeled=source_all+target
+    unlabeled=source_all+target_un
 
     encoder_feats = CountVectorizer(ngram_range=(1, 2), token_pattern=r'\b\w+\b', min_df=20, binary=True)
     X_source_feats = encoder_feats.fit_transform(train)
     X_target_feats = encoder_feats.transform(dest_test)
     X_unlabeled_feats = encoder_feats.transform(unlabeled)
     X_source_valid_feats = encoder_feats.transform(test)
+    y_unlabeled_dom = np.zeros((X_unlabeled_feats.shape[0],1))
+    y_unlabeled_dom[:len(source_all),:] += 1
+
+    X_tgtun_feats = encoder_feats.transform(target_un)
+    y_tgtun_dom = np.zeros((X_tgtun_feats.shape[0],1))
 
     encoder_ae = CountVectorizer(ngram_range=(1, 2), token_pattern=r'\b\w+\b', min_df=un_count, binary=True)
     X_unlabeled_ae = encoder_ae.fit_transform(unlabeled)
+    X_tgtun_ae = encoder_ae.transform(target_un)
 
-    X_unlabeledonly_ae = encoder_ae.transform(unlabeled_only)
+    # X_unlabeledonly_ae = encoder_ae.transform(unlabeled_only)
     X_source_ae = encoder_ae.transform(train)
     X_allsource_ae = encoder_ae.transform(source_all)
     X_target_ae = encoder_ae.transform(dest_test)
@@ -327,8 +386,9 @@ def main(args):
                 X_target_ae,
                 ae_input_inds,
                 ae_output_inds,
-                X_unlabeled_feats=X_unlabeled_feats,
-                X_unlabeled_ae=X_unlabeled_ae,
+                X_unlabeled_feats=X_tgtun_feats,
+                X_unlabeled_ae=X_tgtun_ae,
+                y_unlabeled_dom=y_tgtun_dom,
                 y_target=None,  # If we come up with oracle mode this can be non-None
                 X_source_valid_feats=X_source_valid_feats,
                 X_source_valid_ae=X_source_valid_ae,
@@ -342,7 +402,7 @@ def main(args):
     target_X_ae = torch.FloatTensor(X_target_ae[:,ae_input_inds].toarray()).to(device)
     target_y = np.array(dest_test_labels)
     if args.eval == 'pt':
-        target_test_predict_raw,_,_ = best_model(target_X_feats, target_X_ae)
+        target_test_predict_raw,_,_,_,_ = best_model(target_X_feats, target_X_ae)
         target_test_predict = np.round(sigmoid(target_test_predict_raw).data.cpu().numpy())[:,0]
         correct_preds = (target_y == target_test_predict).sum()
         acc = correct_preds / len(target_y)
